138bcd89e00865fa97ab2390cb1809f7de5c0d07
Jan Biały <36222287+yachoo2606@users.noreply.github.com>
2024-04-25 21:22:31 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8080;
 
 app.use(express.static(__dirname));
-app.use('/dist', express.static('dist'));
+app.use(express.static(__dirname + '/dist/'));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 })
